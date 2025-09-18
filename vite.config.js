1a10916051bb340074c2badb28f44320202f4ac9
Marcelo Meloni <132206380+marcelomeloni@users.noreply.github.com>
2025-09-18 10:17:35 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,29 @@
-// Em: vite.config.js
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path' // <-- Importe o 'path'
+import path from 'path'
+import nodePolyfills from 'rollup-plugin-node-polyfills'
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'global': {},
-  },
   resolve: {
     alias: {
-      buffer: 'buffer/',
-
+      buffer: 'buffer',
+      process: 'process/browser',
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  define: {
+    global: 'globalThis',
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: { global: 'globalThis' },
+      plugins: [nodePolyfills()]
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [nodePolyfills()]
     },
   },
 })
