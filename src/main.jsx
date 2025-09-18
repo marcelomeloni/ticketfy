94eed3e2307dc37f6df52accc33a540d85feccc8
Marcelo Meloni <132206380+marcelomeloni@users.noreply.github.com>
2025-09-18 14:38:18 -0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,8 @@
-// Em: src/main.jsx
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
-import { Buffer } from 'buffer';
-import process from 'process';
-
-window.Buffer = Buffer;
-window.process = process;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-import nodePolyfills from 'rollup-plugin-node-polyfills'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    
+    nodePolyfills(), 
+  ],
   resolve: {
+   
     alias: {
-      // Polyfills Node
-      buffer: 'buffer',
-      process: 'process/browser',
-      util: 'rollup-plugin-node-polyfills/polyfills/util',
-      stream: 'rollup-plugin-node-polyfills/polyfills/stream',
-      path: 'rollup-plugin-node-polyfills/polyfills/path',
-      assert: 'rollup-plugin-node-polyfills/polyfills/assert',
       '@': path.resolve(__dirname, './src'),
     },
   },
-  define: {
-    global: 'globalThis',
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: { global: 'globalThis' },
-      plugins: [nodePolyfills()]
-    },
-  },
-  build: {
-    rollupOptions: {
-      plugins: [nodePolyfills()]
-    },
-  },
-})
+});
