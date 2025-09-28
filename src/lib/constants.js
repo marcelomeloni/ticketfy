92637914cf0352a00b77f2bf04008994e235e020
Marcelo Meloni <132206380+marcelomeloni@users.noreply.github.com>
2025-09-28 15:41:12 -0300

    Update constants.js

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -2,10 +2,11 @@ import { web3 } from '@coral-xyz/anchor';
 
 
 export const PROGRAM_ID = new web3.PublicKey(
-  "2jq7fqT3gxn7Xg2SrxNobqUNxPQeWC5B5griJUpEFwvG"
+  "B5guTKgFpRvsqiLNAJb7XAePmoLW9awyLP5BctPRurYo"
 );
 
 
 export const API_URL = "https://gasless-api-ke68.onrender.com";
+
 
 export const APP_BASE_URL = "https://ticketfy.app/"
